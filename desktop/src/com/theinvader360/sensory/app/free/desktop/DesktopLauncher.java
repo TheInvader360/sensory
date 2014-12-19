bfa2408a0ce37d2f31b0326a5879beed3199bb75
Darren <darren.tate@thebookpeople.co.uk>
2014-12-19 15:01:55 +0000

    initial commit

---
+++ desktop/src/com/theinvader360/sensory/app/free/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.theinvader360.sensory.app.free.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theinvader360.sensory.app.free.Sensory;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sensory(), config);
+	}
+}
---
+++ html/src/com/theinvader360/sensory/app/free/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.theinvader360.sensory.app.free.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.theinvader360.sensory.app.free.Sensory;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Sensory();
+        }
+}
